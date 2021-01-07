a1d63f2921f8790983e7bfe51e900dd682888d29
Devjeet Roy <devjeetroy.dr@gmail.com>
2021-01-07 20:04:22 +0530

    Add files via upload

---
+++ JF-Day-2/Example2.java
@@ -0,0 +1,7 @@
+class Example2{
+	public static void main(String args[]){
+		int num;
+		num=100;
+		System.out.println("The name is :"+	num);
+	}
+}
---
+++ JF-Day-2/Example3.java
@@ -0,0 +1,7 @@
+class Example3{
+	public static void main(String args[]){
+		double a=9.0;
+		double c = Math.sqrt(a);
+		System.out.println(c);
+	}
+}
